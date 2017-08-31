3b1581844bb73c9ebf7aecbc6449c4910fed6fbb
cjcj5438 <1002931123@qq.com>
2017-08-31 10:28:47 +0800

    indexOf 扩展

---
+++ js/String01.js
@@ -0,0 +1,20 @@
+/**
+ * JS里面indexOf 方法扩展
+ * includes()：返回布尔值，表示是否找到了参数字符串。
+ * startsWith()：返回布尔值，表示参数字符串是否在原字符串的头部。
+ * endsWith()：返回布尔值，表示参数字符串是否在原字符串的尾部。
+ */
+var s = 'Hello world!';
+
+s.startsWith('Hello') // true
+s.endsWith('!') // true
+s.includes('o') // true
+
+//这三个方法都支持第二个参数，表示开始搜索的位置。
+
+var s = 'Hello world!';
+
+s.startsWith('world', 6) // true
+s.endsWith('Hello', 5) // true
+s.includes('Hello', 6) // false
+//上面代码表示，使用第二个参数n时，endsWith的行为与其他两个方法有所不同。它针对前n个字符，而其他两个方法针对从第n个位置直到字符串结束。
